3dc0d72eef8824e5e5eaef734ce8afd87f7bda41
reika <r.okinu.513@gmail.com>
2025-02-17 07:33:33 +0900

    file commit

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MogitateController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +13,5 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+s
+Route::get('/products' [MogitateController::class, 'index']);
